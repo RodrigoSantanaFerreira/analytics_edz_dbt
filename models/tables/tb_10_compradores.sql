db93b1a889dd60575c647c00f5555efeba1f1267
Rodrigo <rodrigodb28@gmail.com>
2022-05-23 19:57:42 +0000

    Adicionando novos modelos

--- models/tables/tb_10_compradores.sql
+++ models/tables/tb_10_compradores.sql
@@ -2,25 +2,23 @@ with sales as (
 
     select * from {{ ref('vw_sales') }}
 
-)
-
+),
 compradores as (
 
     SELECT
-        (firstname ||' '|| lastname) as Nome_Completo,
-        userid
+        (firstname ||' '|| lastname) as Nome_Completo, userid
     FROM users
-)
+),
 
 top_10_compradores as (
 
     SELECT
         compradores.Nome_Completo as Nome_Completo,
         sum(sales.quantidade_vendida) as Vendas_totais,
-        sales.comisão as Comissão
+        sales.comissao as Comissao
     FROM sales INNER JOIN compradores
     ON sales.id_comprador = compradores.userid
-    GROUP BY compradores.userid
+    GROUP BY compradores.userid, compradores.Nome_Completo, sales.comissao
     ORDER BY Vendas_totais DESC
     LIMIT 10
 
---
+++ target/compiled/analytics_dbt/models/tables/tb_vendas_primeiro_trimestre.sql
@@ -0,0 +1,24 @@
+-- define o as variáveis meses e ano
+
+
+
+-- cria a fonte com os dados de date e converte o  nome das colunas
+with source_date as (
+    select  dateid as id_date,
+            month as mes,
+            year as ano
+    from date
+
+),
+-- cria uma fonte de dados unindo a tabela sales com date
+sales_date as (
+
+    select  source_date.mes, 
+            sum(quantidade_vendida)
+    from  "dev"."public"."vw_sales" sales inner join source_date
+    on sales.id_date = source_date.id_date
+    where source_date.mes in ('JAN', 'FEB', 'MAR')
+    and source_date.ano = '2008'
+    group by source_date.mes
+)
+select * from sales_date
